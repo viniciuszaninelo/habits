ec11ef9d1b5af1647930e2ad1eb536d72cee7879
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2023-02-03 11:56:20 -0300

    componentizing elements for the web

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,20 @@
+
+import { Plus } from 'phosphor-react'
+import LogoImg from '../assets/logo.svg'
+
+
+export function Header () {
+  return (
+    <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+      <img src={LogoImg} alt="logo habits" />
+      
+      <button
+        type='button'
+        className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex center gap-3 hover:border-violet-300'
+      >
+        <Plus size={20} className='text-violet-500'/>
+        Novo hábito
+      </button>
+    </div>
+  )
+}
